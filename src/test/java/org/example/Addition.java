18533a4b5fc323434d78ec09b49a96d0b04c3739
Roopa <roopaworld@yahoo.com>
2020-04-25 10:45:48 +0100

    changed integer

--- src/test/java/org/example/Addition.java
+++ src/test/java/org/example/Addition.java
@@ -8,11 +8,12 @@ public class Addition {
 
 
     public static void main(String[] args) {
-
+        int c =17;
         a = 5;
         b = 6;
 
         System.out.println("Hello sum of two numbers is :" + a + b);
+        System.out.println(a+b+c);
 
     }
 }
