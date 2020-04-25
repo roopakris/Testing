24d2a0ceb0ff60d34848e46e87e338991441f1b6
Roopa <roopaworld@yahoo.com>
2020-04-25 10:55:29 +0100

    mutliplication

---
+++ src/test/java/org/example/Multiplication.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Multiplication {
+    public static void main(String[] args){
+
+        int a=10;
+        int b=12;
+        int c =20;
+        System.out.println(a*b);
+        System.out.println(a*b*c);
+    }
+}
