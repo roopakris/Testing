280a228b9dbc0194e101f4bc391926fe4622730e
Roopa <roopaworld@yahoo.com>
2020-04-25 10:38:21 +0100

    first commit

---
+++ src/test/java/org/example/Addition.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Addition {
+
+    static int a;
+    static int b;
+
+
+
+    public static void main(String[] args) {
+
+        a = 5;
+        b = 6;
+
+        System.out.println("Hello sum of two numbers is :" + a + b);
+
+    }
+}
